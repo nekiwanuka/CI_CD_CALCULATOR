06dfddb940b4dd2b17528bd76ffc1040ed052d5e
Aloisius <kalosius43@gmail.com>
2023-07-04 14:36:26 +0300

    Added the error messages in case of wrong syntax

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Calculation
+from django.contrib import messages
 
 def calculator(request):
     calculation_history = Calculation.objects.order_by('-id')[:5]
@@ -19,14 +20,15 @@ def calculator(request):
             if num2 != 0:
                 result = num1 / num2
             else:
-                result = "Error: Cannot divide by zero"
+                messages.error(request, 'Error: Cannot divide by zero')
+                return redirect('calculator')
         else:
-            result = "Invalid operator"
+            messages.error(request, 'Invalid Operator')
+            return redirect('calculator')
 
         # Save current calculation to the database
         calculation = Calculation(expression=f"{num1} {operator} {num2}", result=result)
         calculation.save()
-
         return redirect('calculator')
 
     return render(request, 'calculator.html', {'history': calculation_history})
